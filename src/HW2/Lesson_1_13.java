4f760429af243973e9f3256ffc222baac2d08710
MaxDobryanskiy <dobriym101@gmail.com>
2025-07-23 17:52:54 +0300

    Commit

---
+++ src/HW2/Lesson_1_13.java
@@ -0,0 +1,13 @@
+package HW2;
+
+public class Lesson_1_13 {
+    public static void main(String[] args) {
+        int n = 25;
+        int[][] squareArray = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            squareArray[i][n - 1 - i] = 1;
+            squareArray[i][i] = 1;
+        }
+        System.out.println(squareArray[0][24]);
+    }
+}
---
+++ src/HW2/Lesson_1_14.java
@@ -0,0 +1,7 @@
+package HW2;
+
+public class Lesson_1_14 {
+    public static void main(String[] args) {
+
+    }
+}

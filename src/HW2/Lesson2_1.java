9cefc2f34edd0ef533649f7441a350328d0d6314
MaxDobryanskiy <dobriym101@gmail.com>
2025-07-29 14:18:14 +0300

    1

---
+++ src/HW2/Lesson2_1.java
@@ -0,0 +1,9 @@
+package HW2;
+
+public class Lesson2_1 {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
---
+++ src/HW2/Lesson2_3.java
@@ -0,0 +1,4 @@
+package HW2;
+
+public class Lesson2_3 {
+}

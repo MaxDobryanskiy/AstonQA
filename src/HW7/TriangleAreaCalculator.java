58a2e993b29764c758ede0ea855454160b01381f
MaxDobryanskiy <dobriym101@gmail.com>
2025-08-14 13:56:20 +0300

    1

--- src/HW7/TriangleAreaCalculator.java
+++ src/HW7/TriangleAreaCalculator.java
@@ -3,7 +3,7 @@ package HW7;
 
 public class TriangleAreaCalculator {
     public static double calculateArea(double base, double height) {
-        if (base <= 0 || height <= 0) throw new IllegalArgumentException("Base and height must be positive");
+        if (base <= 0 || height <= 0) throw new IllegalArgumentException("Значения должны быть положительными.");
         return (base * height) / 2;
     }
 }
